36192402858944efe64339fdcd6a8333850b00ce
TahmidTaki <tahmid.taki@live.iium.edu.my>
2022-11-26 23:01:16 +0800

    books for relase add api created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
@@ -15,3 +16,42 @@ app.get("/", async (req, res) => {
 app.listen(port, () => {
   console.log(`book resale running at port ${port}`);
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bvgttje.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const usersCollection = client.db("booksResale").collection("users");
+    const booksCollection = client.db("booksResale").collection("books");
+    const categoriesCollection = client.db("booksResale").collection("bookCategories");
+
+    //create user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    //get categories
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const result = await categoriesCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //post add books for resale
+    app.post("/books", async (req, res) => {
+      const book = req.body;
+      const result = await booksCollection.insertOne(book);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
